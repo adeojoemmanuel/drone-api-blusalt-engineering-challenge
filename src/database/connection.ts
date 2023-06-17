b8fefd2478db61d3902c35abce6ea9c09191c898
adeojoemmanuel3 <emmanuel.adeojo@qodeup.com>
2023-06-17 18:46:57 +0100

    fix: fix mongodb connection error

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,11 +3,7 @@ import mongoose from 'mongoose';
 // Replace 'mongodb://localhost:27017/drones' with your MongoDB connection string
 const connectionString = 'mongodb://localhost:27017/drones';
 
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(connectionString, {});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
