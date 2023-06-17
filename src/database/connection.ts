cf91864dc80ffdac6d481dcaf974135e63844bdd
adeojoemmanuel3 <emmanuel.adeojo@qodeup.com>
2023-06-17 18:58:31 +0100

    feat: add files to be ignored

--- src/database/connection.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-// Replace 'mongodb://localhost:27017/drones' with your MongoDB connection string
-const connectionString = 'mongodb://localhost:27017/drones';
-
-mongoose.connect(connectionString, {});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
